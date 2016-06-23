7025ea12d99eac1a6795b771e28443b13d2cfcd2
Moisés López <moylop260@vauxoo.com>
2016-06-23 23:56:19 +0000

    clean 2 with infinite loop

--- account_financial_report/report/parser.py
+++ account_financial_report/report/parser.py
@@ -1,37 +1,3 @@
-# coding: utf-8
-###########################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
-#    All Rights Reserved
-# Credits######################################################
-#    Coded by:   Humberto Arocha <hbto@vauxoo.com>
-#                Angelica Barrios angelicaisabelb@gmail.com
-#               Jordi Esteve <jesteve@zikzakmedia.com>
-#               Javier Duran <javieredm@gmail.com>
-#    Planified by: Humberto Arocha
-#    Finance by: LUBCAN COL S.A.S http://www.lubcancol.com
-#    Audited by: Humberto Arocha humberto@openerp.com.ve
-#############################################################################
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
-
-import time
-from openerp.report import report_sxw
-from openerp.tools.translate import _
-from openerp.osv import osv
-
-
 class AccountBalance(report_sxw.rml_parse):
     _name = 'afr.parser'
 
