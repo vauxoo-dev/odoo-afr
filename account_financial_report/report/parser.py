b525549e73964b5b97b51eba2b50882ee934f900
Moisés López <moylop260@vauxoo.com>
2016-06-24 00:13:45 +0000

    clean200 with infinite loop

--- account_financial_report/report/parser.py
+++ account_financial_report/report/parser.py
@@ -1,9 +1,5 @@
 def lines(self, form, level=0):
-    def _get_children_and_consol():
-            ids2 = []
-            ids2 += _get_children_and_consol(
-                        cr, uid, [x.id for x in aa_brw.child_id], level,
-                        context, change_sign=change_sign)
-            return ids2
-
-
+    def _get_children_and_consol(my_list):
+        ids2 = []
+        ids2 += _get_children_and_consol([x.id for x in my_list])
+        return ids2
