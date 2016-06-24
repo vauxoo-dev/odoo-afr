6ef69e71078420fb411453a110316cf8c9f776f6
Moisés López <moylop260@vauxoo.com>
2016-06-24 00:03:55 +0000

    clean 6 with infinite loop

--- account_financial_report/report/parser.py
+++ account_financial_report/report/parser.py
@@ -22,10 +22,6 @@ class AccountBalance(report_sxw.rml_parse):
                     ids2 += _get_children_and_consol(
                         cr, uid, [x.id for x in aa_brw.child_id], level,
                         context, change_sign=change_sign)
-                    if change_sign:
-                        ids2.append(aa_brw.id)
-                    else:
-                        ids2.append([aa_brw.id, False, True, aa_brw])
                 else:
                     if change_sign:
                         ids2.append(aa_brw.id)
