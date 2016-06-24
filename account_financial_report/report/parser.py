0fbbb92ea4557736a8ef45bc32489709744e8e8f
Moisés López <moylop260@vauxoo.com>
2016-06-24 00:09:23 +0000

    clean108 with infinite loop

--- account_financial_report/report/parser.py
+++ account_financial_report/report/parser.py
@@ -1,25 +1,7 @@
-class AccountBalance(report_sxw.rml_parse):
-    _name = 'afr.parser'
-
-    def lines(self, form, level=0):
-        """
-        Returns all the data needed for the report lines (account info plus
-        debit/credit/balance in the selected period and the full year)
-        """
-        account_obj = self.pool.get('account.account')
-        period_obj = self.pool.get('account.period')
-        fiscalyear_obj = self.pool.get('account.fiscalyear')
-
-        def _get_children_and_consol(cr, uid, ids, level, context=None,
-                                     change_sign=False):
-            aa_obj = self.pool.get('account.account')
+def lines(self, form, level=0):
+    def _get_children_and_consol():
             ids2 = []
-            for aa_brw in aa_obj.browse(cr, uid, ids, context):
-                if aa_brw.child_id and aa_brw.level < \
-                        level and aa_brw.type != 'consolidation':
-                    if not change_sign:
-                        ids2.append([aa_brw.id, True, False, aa_brw])
-                    ids2 += _get_children_and_consol(
+            ids2 += _get_children_and_consol(
                         cr, uid, [x.id for x in aa_brw.child_id], level,
                         context, change_sign=change_sign)
             return ids2
